a88f7074a1d41ca9ed9414cb0a0a7390d8698c28
ds300 <ds300@sussex.ac.uk>
2013-02-04 10:48:42 +0000

    setup leiningen template

---
+++ src/norm/core.clj
@@ -0,0 +1,6 @@
+(ns norm.core)
+
+(defn -main
+  "I don't do a whole lot."
+  [& args]
+  (println "Hello, World!"))
---
+++ test/norm/core_test.clj
@@ -0,0 +1,7 @@
+(ns norm.core-test
+  (:use clojure.test
+        norm.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
