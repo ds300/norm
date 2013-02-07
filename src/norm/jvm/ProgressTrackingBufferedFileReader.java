73d7b449607f3b3287d9fffdfad79ff339e26720
David Sheldrick <ds300@sussex.ac.uk>
2013-02-07 20:11:43 +0000

    moved jvm sources

---
+++ src/norm/jvm/ProgressTrackingBufferedFileReader.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.nio.*;
+import java.nio.channels.*;
+public class ProgressTrackingBufferedFileReader extends BufferedReader {
+  private FileChannel channel;
+  private long size;
+  public ProgressTrackingBufferedFileReader (Reader in) {
+    super(in);
+  }
+
+  public static ProgressTrackingBufferedFileReader make (String filename) {
+    try {
+      File thefile = new File(filename);
+      long size = thefile.length();
+      FileInputStream stream = new FileInputStream(thefile);
+      FileChannel channel = stream.getChannel();
+      ProgressTrackingBufferedFileReader ret = new ProgressTrackingBufferedFileReader(new InputStreamReader(stream));
+      ret.channel = channel;
+      ret.size = size;
+      return ret;
+    } catch (FileNotFoundException e) {
+      e.printStackTrace();
+      System.exit(1);
+      return null; 
+    }
+  }
+
+  public float progress() {
+    try {
+      return (100f / size) * channel.position();
+    } catch (IOException e) {return 100f;}
+    
+  }
+}
