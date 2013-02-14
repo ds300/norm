9115f477c2f2f38910f4ea995f2428773d669967
ds300 <ds300@sussex.ac.uk>
2013-02-14 20:03:36 +0000

    a little brevity goes a long way

--- src/norm/train/tlm.clj
+++ src/norm/train/tlm.clj
@@ -2,13 +2,13 @@
   (:require [norm.data :as data]
             [norm.io :as io]))
 
-
 (defn train []
+  (let [tmp_path (str io/OUT_PATH ".tmp")]
+    
+    (edu.berkeley.nlp.lm.io.MakeKneserNeyArpaFromText/main
+      (into-array String ["3" tmp_path (data/get-path :twt-c)]))
 
-  (edu.berkeley.nlp.lm.io.MakeKneserNeyArpaFromText/main
-    (into-array String ["3" (str io/OUT_PATH ".tmp") (data/get-path :twt-c)]))
+    (edu.berkeley.nlp.lm.io.MakeLmBinaryFromArpa/main
+      (into-array String ["-h" tmp_path io/OUT_PATH]))
 
-  (edu.berkeley.nlp.lm.io.MakeLmBinaryFromArpa/main
-    (into-array String ["-h" (str io/OUT_PATH ".tmp") io/OUT_PATH]))
-
-  (.delete (java.io.File. (str io/OUT_PATH ".tmp"))))
+    (.delete (java.io.File. tmp_path))))
